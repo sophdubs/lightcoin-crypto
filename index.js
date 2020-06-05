092e4122e3c0fe3f68828626c1c39febdac415db
Sophie <sophiemargueritedubois@gmail.com>
2020-06-05 02:02:54 +0000

    Successfully refactored the code to use classes and OOP, resulting in a more efficient and DRY project

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor() {
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    if (this.isAllowed()) {
+      this.account.addTransaction(this);
+      return true;
+    }
+    return false;
+  }
+
+}
+
+
+
+class Withdrawal extends Transaction{
+
+  get value(){
+    return -this.amount;
+  }
+
+  isAllowed() {
+    if (this.account.balance + this.value < 0) {
+      return false;
+    }
+    return true;
+  }
+
+}
+
+class Deposit extends Transaction{
+
+  get value(){
+    return this.amount;
+  }
+
+  isAllowed() {
+    return true;
   }
 
 }
@@ -15,15 +69,33 @@ class Withdrawal {
 
 
 
+
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-patrol');
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+// console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+t2 = new Withdrawal(9.99, myAccount);
+let result = t2.commit();
+console.log('rejected withdrawal result:', result);
+// console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+// console.log('Balance:', myAccount.balance);
+
+t3 = new Deposit(120.00, myAccount);
+let result2 = t3.commit();
+console.log('deposit result:', result2);
+// console.log('Transaction 3:', t3);
+
+// console.log('Balance:', myAccount.balance);
+t4 = new Withdrawal(100.00, myAccount);
+let result3 = t4.commit();
+console.log('approved withdrawal result:', result3);
+
+
+console.log(myAccount.balance);
+
